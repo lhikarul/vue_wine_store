d72e5e74f7980e3ea73f47379bdc91da7dc75210
Evans <711hikaru@gmail.com>
2019-11-29 20:00:09 +0800

    add: header/footer 組件 mobile

---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="footer-area">
+      <div class="img-wrapper">
+           <img class="logo-img" src="https://colorlib.com/preview/theme/wines/images/logo.png" alt="logo">
+      </div>
+      <div class="footer-info">
+          <div class="footer-info-phone">07-1234-5678</div>
+          <div class="footr-info-email">wines@email.com</div>
+          <div class="footer-info-address">800 高雄市新興區幸福路520號</div>
+      </div>
+      <div class="social-icons">
+            <i class="fab fa-instagram"></i>
+            <i class="fab fa-facebook-square"></i>
+      </div>
+      <div class="copyright">© 1885 ~ 2019 wines* All Rights Reserved</div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'VFooter'
+}
+</script>
+
+<style lang="scss" scoped>
+    .footer-area {
+        padding: 30px;
+        background: #EAF0ED;
+        color: #3F5D45;
+
+        .img-wrapper {
+            margin-top: 9px;
+        }
+
+        .footer-info {
+            margin-top: 39.5px;
+            margin-bottom: 37px;
+            line-height: 1.5;
+        }
+
+        .social-icons i {
+            font-size: 32px;
+
+            &:first-child {
+                margin-right: 8px;
+            }
+        }
+
+        .copyright {
+            margin-top: 23px;
+        }
+    }
+</style>
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,39 @@
+<template>
+    <header class="header">
+        <div class="header-left">
+            <i class="fas fa-bars icon"></i>
+        </div>
+
+        <div class="header-middle">
+            <img class="logo-img" src="https://colorlib.com/preview/theme/wines/images/logo.png" alt="logo">
+        </div>
+
+        <div class="header-right">
+            <i class="fas fa-shopping-cart icon"></i>
+        </div>
+
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'VHeader'
+}
+</script>
+
+<style lang="scss" scoped>
+    .header {
+        padding: 30px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        
+        .logo-img {
+            width: 140px;
+        }
+
+        .icon {
+            font-size: 24px;
+        }
+    }
+</style>
