c7c65cf00099ad7674f491da642bd367af918e9a
Evans <711hikaru@gmail.com>
2019-11-30 20:36:27 +0800

    edit:: Email pc layout

--- src/components/Home/Email.vue
+++ src/components/Home/Email.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="email-area">
-      <div class="email-wrapper">
-          <div class="email-info-wrapper">
-              <div class="img-wrapper">
-                  <img src="https://previews.123rf.com/images/topvectors/topvectors1712/topvectors171200354/91202652-wine-label-%C3%B1%C6%92%C3%B1%E2%80%B9%C3%B0%C2%B5%C3%B0%C2%B2-1935-natural-top-quality-product-vintage-logo-design-element-for-menu-winery-log.jpg" alt="brand logo">
-              </div>
-              <h3 class="email-info-text">訂閱你我的甜蜜郵件</h3>
-          </div>
-          <div class="input-wrapper">
-              <span><i class="fas fa-envelope"></i></span>
-              <input class="email" type="email" />
-              <span class="icon-arrow"><i class="fas fa-arrow-right"></i></span>
-          </div>
-      </div>
+        <div class="email-wrapper">
+            <div class="email-info-wrapper">
+                <div class="img-wrapper">
+                    <img src="https://previews.123rf.com/images/topvectors/topvectors1712/topvectors171200354/91202652-wine-label-%C3%B1%C6%92%C3%B1%E2%80%B9%C3%B0%C2%B5%C3%B0%C2%B2-1935-natural-top-quality-product-vintage-logo-design-element-for-menu-winery-log.jpg" alt="brand logo">
+                </div>
+                <h3 class="email-info-text">訂閱你我的甜蜜郵件</h3>
+            </div>
+            <div class="input-wrapper">
+                <span><i class="fas fa-envelope"></i></span>
+                <input class="email" type="email" />
+                <span class="icon-arrow"><i class="fas fa-arrow-right"></i></span>
+            </div>
+        </div>
   </div>
 </template>
 
@@ -24,15 +24,15 @@ export default {
 
 <style lang="scss" scoped>
     .email-area {
-        margin-top: 30px;
+
         padding: 30px;
-        background: #3F5D45;
 
         .email-info-wrapper {
             
             padding: 0 20px;
             display: flex;
             align-items: center;
+            justify-content: center;
 
             .img-wrapper img {
                 border-radius: 50%;
@@ -65,9 +65,33 @@ export default {
 
             .icon-arrow {
                 flex-basis: 64px;
+                width: 64px;
                 background: #FFE180;
                 color: #3F5D45;
                 text-align: center;
+            }
+        }
+    }
+
+    @media (min-width: 940px) {
+        .email-area {
+            .email-wrapper {
+                padding: 0 15%;
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+
+                .email-info-wrapper {
+                    padding: 0;
+
+                    .email-info-text {
+                        font-size: 24px;
+                    }
+                }
+
+                .input-wrapper {
+                    margin-top: 0;
+                }
             }
         }
     }
