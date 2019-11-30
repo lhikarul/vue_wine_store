8197df4114db2116df8f547e01f26b23fd4f9a02
Evans <711hikaru@gmail.com>
2019-11-30 20:37:48 +0800

    edit:: Home/Title pc layout

--- src/components/Home/Title.vue
+++ src/components/Home/Title.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-        <h1 class="heading-title">{{title}}-----
+        <h1 class="heading-title"><span class="position">{{title}}-----</span>
             <br>
             {{subTitle}}
         </h1>
@@ -26,11 +26,23 @@ export default {
 <style lang="scss" scoped>
 
 .heading-title {
-    margin: 30px 0;
+    padding-bottom: 30px;
     color: #3F5D45;
     font-size: 24px;
     letter-spacing: 2px;
     text-align: center;
+}
+
+@media (min-width: 940px) {
+    .heading-title {
+        padding: 0;
+        font-size: 32px;
+        text-align: left;
+    }
+
+    .position {
+        margin-top: -20px;
+    }
 }
 
 </style>
